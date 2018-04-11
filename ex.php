820a87cde41cb7591970a0f2241bc13dd83fc1ee
ƬHE ЯAW ☣ <raw@dopehosting.net>
2018-04-11 12:10:10 +0200

    Update ex.php

--- ex.php
+++ ex.php
@@ -1,15 +1,19 @@
 <?php
 $user = 'raw';
 $uidd = '1002';
+$vfs = '/kvm/userfs';
 
 $filescreatedMaxLimit = '30,000';
 $maxNPROC = '100';
 $maxRAM = '16018MB';
+$maxSPACE = '5G';
 
-$liveF = exec("find /kvm/userfs/home/$user/ -type f | wc -l");
+$liveF = exec("find $vfs/home/$user/ -type f | wc -l");
 $livePROC = exec("ps aux | grep $user |  wc -l");
 $liveRAM = exec("ps -U $user --no-headers -o rss | awk '{ sum+=$1} END {print int(sum/1024) \"MB\"}'");
+$liveSPACE = exec("du -sh $vfs/home/$user | cut -f1");
 echo "RAW TOTAL FILES MAX/LIVE : $filescreatedMaxLimit/$liveF";
 echo "RAW MAX LIVE PROCC : $maxNPROC/$livePROC";
 echo "RAW MAX LIVE RAM : $maxRAM/$liveRAM";
+echo9 "RAW MAX LIVE SPACE : $maxSPACE/$liveSPACE";
 ?>
